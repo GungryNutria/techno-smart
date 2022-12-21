ea171696a70b56a88dd6730a0b48f604c2bfe5f8
GungryNutria <edgardochamorro08@gmail.com>
2022-12-21 11:57:01 -0600

    inicio de repositorio

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Techno Smart</title>
+</head>
+<body>
+    <?php
+        include 'src/pages/home_page.php'
+    ?>
+</body>
+</html>
