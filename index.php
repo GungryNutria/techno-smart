86369d6c83b5eedd6d56655c09b2076e299b2431
GungryNutria <edgardochamorro08@gmail.com>
2022-12-21 21:02:43 -0600

    Banner principal

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Techno Smart</title>
     <?php
-    include 'libraires.php';
+    include 'src/shared/libraries.php';
     ?>
 </head>
 <body>
--- src/shared/libraries.php
+++ src/shared/libraries.php
@@ -1,1 +1,2 @@
 <link rel="stylesheet" href="src/css/styles.css">
+<link rel="stylesheet" href="src/css/home_section_hero.css">
