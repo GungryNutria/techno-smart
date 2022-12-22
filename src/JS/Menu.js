63e1ea66acf352c8eed8b7d24b7d77a6db7a698f
Juan Antonio Torres <95553786+jatoni@users.noreply.github.com>
2022-12-21 22:19:44 -0600

    Se queda al 100 menu despeglable, hay que agregar algunas animaciones

---
+++ src/JS/Menu.js
@@ -0,0 +1,8 @@
+const desplegar = () => {
+    const despegable = document.getElementById('menu-despegable');
+    despegable.style.display = 'flex';
+}
+const noDespeglar = () => {
+    const despegable = document.getElementById('menu-despegable');
+    despegable.style.display = 'none';
+}
--- src/shared/libraries.php
+++ src/shared/libraries.php
@@ -4,3 +4,4 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@500;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="src/css/home_section_hero.css">
+<link rel="shortcut icon" href="SRC/assets/logo.png" type="image/x-icon">
