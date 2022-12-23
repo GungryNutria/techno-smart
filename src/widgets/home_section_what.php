a3cce587ca2c5b96b9477a82b4a209acb1e5e90b
GungryNutria <edgardochamorro08@gmail.com>
2022-12-23 10:22:09 -0600

    Avance en home page

--- src/widgets/home_section_what.php
+++ src/widgets/home_section_what.php
@@ -4,24 +4,52 @@
     <br>
     <div class="what-container">
         <div class="what-box">
-            <div class="what-img"></div>
-            <div class="what-title"></div>
+            <a href="#">
+                <img src="src/assets/what1.png" alt="">
+                <p>Ahorro de Energia</p>
+            </a>
         </div>
         <div class="what-box">
-            <div class="what-img"></div>
-            <div class="what-title"></div>
+            <a href="#">
+                <img src="src/assets/what2.png" alt="">
+                <p>Seguridad</p>
+            </a>
         </div>
         <div class="what-box">
-            <div class="what-img"></div>
-            <div class="what-title"></div>
+            <a href="#">
+                <img src="src/assets/what3.png" alt="">
+                <p>Aditivos y Anticongelantes</p>
+            </a>
         </div>
         <div class="what-box">
-            <div class="what-img"></div>
-            <div class="what-title"></div>
+            <a href="#">
+                <img src="src/assets/what4.png" alt="">
+                <p>Aceros Balisticos</p>
+            </a>
         </div>
         <div class="what-box">
-            <div class="what-img"></div>
-            <div class="what-title"></div>
+            <a href="#">
+                <img src="src/assets/what5.png" alt="">
+                <p>Desarrollo de Productos Medicos</p>
+            </a>
+        </div>
+        <div class="what-box">
+            <a href="#">
+                <img src="src/assets/what6.png" alt="">
+                <p>Desarrollo de Armamento</p>
+            </a>
+        </div>
+        <div class="what-box">
+            <a href="#">
+                <img src="src/assets/what7.png" alt="">
+                <p>Desarrollo de Vehiculos no Tripulados</p>
+            </a>
+        </div>
+        <div class="what-box">
+            <a href="#">
+                <img src="src/assets/what8.png" alt="">
+                <p>Automatizacion</p>
+            </a>
         </div>
     </div>
 </div>
