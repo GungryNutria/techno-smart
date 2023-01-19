afb25064441f20f5a4514dc956aff238425e2723
GungryNutria <edgardochamorro08@gmail.com>
2023-01-19 16:51:09 -0600

    sin footer

--- src/shared/footer.php
+++ src/shared/footer.php
@@ -3,27 +3,6 @@
     <div class="logo-container">
         <img src="src/assets/logo.png" alt="">
     </div>
-    <div class="contact-container">
-        <div class="contact-box">
-            <img src="src/assets/footer1.png" alt="">
-            <p class="contact-value">IntGen Technologies de México, Coacalco de Berriozabal, Estado de México</p>
-            <p class="contact-title">Nuestra Ubicación</p>
-        </div>
-        <div class="contact-box">
-            <img src="src/assets/footer2.png" alt="">
-            <p class="contact-value">proyectos@intgen.com.mx</p>
-            <p class="contact-title">Nuestro Correo</p>
-        </div>
-        <div class="contact-box">
-            <img src="src/assets/footer3.png" alt="">
-            <p class="contact-value">5562169478 | 5585824030</p>
-            <p class="contact-title">Nuestro Celular</p>
-
-        </div>
-    </div>
-    <div class="copyright-container">
-        <p>Copyright © 2023 Todos los derechos reservados</p>
-    </div>
-    <div class="social-container"></div>
+    
 </footer>
 </center>
