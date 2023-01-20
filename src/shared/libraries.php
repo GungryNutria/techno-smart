83d389c5ce9a6df7f448743aec2f652fd8bfd881
GungryNutria <edgardochamorro08@gmail.com>
2023-01-19 18:28:17 -0600

    Cambio de nombre de archivos

--- src/shared/libraries.php
+++ src/shared/libraries.php
@@ -2,7 +2,7 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@500;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="src/css/styles.css">
-<link rel="stylesheet" href="src/css/menu.css">
+<link rel="stylesheet" href="src/css/Menu.css">
 <link rel="stylesheet" href="src/css/footer.css">
 <link rel="stylesheet" href="src/css/burbujas_section_hero.css">
 <link rel="stylesheet" href="src/css/home_section_hero.css">
